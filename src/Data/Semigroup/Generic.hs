b662fb81885b06f17163d57dd0ba0a68c52b1f47
Edward Kmett <ekmett@gmail.com>
2015-03-06 17:11:33 -0500

    fold Data.Monoid.Generic into Data.Semigroup.Generic

--- src/Data/Semigroup/Generic.hs
+++ src/Data/Semigroup/Generic.hs
@@ -4,6 +4,8 @@
 module Data.Semigroup.Generic
   ( GSemigroup
   , genericMappend
+  , GMonoid
+  , genericMempty
   ) where
 
 import Data.Semigroup
@@ -13,6 +15,10 @@ import GHC.Generics
 -- implementing 'Generic'. This operation will append two values
 -- by point-wise appending their component fields. It is only defined
 -- for product types.
+--
+-- @
+-- 'genericMappend' a ('genericMappend' b c) = 'genericMappend' ('genericMappend' a b) c
+-- @
 genericMappend :: (Generic a, GSemigroup (Rep a)) => a -> a -> a
 genericMappend x y = to (gmappend (from x) (from y))
 
@@ -33,3 +39,30 @@ instance GSemigroup f => GSemigroup (M1 i c f) where
 
 instance (GSemigroup f, GSemigroup g) => GSemigroup (f :*: g) where
   gmappend (x1 :*: x2) (y1 :*: y2) = gmappend x1 y1 :*: gmappend x2 y2
+
+-- | Generically generate a 'Monoid' 'mempty' for any product-like type
+-- implementing 'Generic'.
+--
+-- It is only defined for product types.
+--
+-- @
+-- 'genericMappend' 'genericMempty' a = a = 'genericMappend' a 'genericMempty'
+-- @
+
+genericMempty :: (Generic a, GMonoid (Rep a)) => a
+genericMempty = to gmempty
+
+class GSemigroup f => GMonoid f where
+  gmempty :: f p
+
+instance GMonoid U1 where
+  gmempty = U1
+
+instance (Semigroup a, Monoid a) => GMonoid (K1 i a) where
+  gmempty = K1 mempty
+
+instance GMonoid f => GMonoid (M1 i c f) where
+  gmempty = M1 gmempty
+
+instance (GMonoid f, GMonoid g) => GMonoid (f :*: g) where
+  gmempty = gmempty :*: gmempty
