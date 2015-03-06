ff20314335c7c26c46d8476ac8ca6e5998856210
Edward Kmett <ekmett@gmail.com>
2015-03-06 17:15:44 -0500

    take the nicer name for the exposed method

--- src/Data/Semigroup/Generic.hs
+++ src/Data/Semigroup/Generic.hs
@@ -17,9 +17,9 @@
 ----------------------------------------------------------------------------
 module Data.Semigroup.Generic
   ( GSemigroup
-  , genericMappend
+  , gmappend
   , GMonoid
-  , genericMempty
+  , gmempty
   ) where
 
 import Data.Semigroup
@@ -31,28 +31,28 @@ import GHC.Generics
 -- for product types.
 --
 -- @
--- 'genericMappend' a ('genericMappend' b c) = 'genericMappend' ('genericMappend' a b) c
+-- 'gmappend' a ('gmappend' b c) = 'gmappend' ('gmappend' a b) c
 -- @
-genericMappend :: (Generic a, GSemigroup (Rep a)) => a -> a -> a
-genericMappend x y = to (gmappend (from x) (from y))
+gmappend :: (Generic a, GSemigroup (Rep a)) => a -> a -> a
+gmappend x y = to (gmappend' (from x) (from y))
 
 class GSemigroup f where
-  gmappend :: f p -> f p -> f p
+  gmappend' :: f p -> f p -> f p
 
 instance GSemigroup U1 where
-  gmappend _ _ = U1
+  gmappend' _ _ = U1
 
 instance GSemigroup V1 where
-  gmappend x y = x `seq` y `seq` error "GSemigroup.V1: gmappend"
+  gmappend' x y = x `seq` y `seq` error "GSemigroup.V1: gmappend'"
 
 instance Semigroup a => GSemigroup (K1 i a) where
-  gmappend (K1 x) (K1 y) = K1 (x <> y)
+  gmappend' (K1 x) (K1 y) = K1 (x <> y)
 
 instance GSemigroup f => GSemigroup (M1 i c f) where
-  gmappend (M1 x) (M1 y) = M1 (gmappend x y)
+  gmappend' (M1 x) (M1 y) = M1 (gmappend' x y)
 
 instance (GSemigroup f, GSemigroup g) => GSemigroup (f :*: g) where
-  gmappend (x1 :*: x2) (y1 :*: y2) = gmappend x1 y1 :*: gmappend x2 y2
+  gmappend' (x1 :*: x2) (y1 :*: y2) = gmappend' x1 y1 :*: gmappend' x2 y2
 
 -- | Generically generate a 'Monoid' 'mempty' for any product-like type
 -- implementing 'Generic'.
@@ -60,23 +60,23 @@ instance (GSemigroup f, GSemigroup g) => GSemigroup (f :*: g) where
 -- It is only defined for product types.
 --
 -- @
--- 'genericMappend' 'genericMempty' a = a = 'genericMappend' a 'genericMempty'
+-- 'gmappend' 'gmempty' a = a = 'gmappend' a 'gmempty'
 -- @
 
-genericMempty :: (Generic a, GMonoid (Rep a)) => a
-genericMempty = to gmempty
+gmempty :: (Generic a, GMonoid (Rep a)) => a
+gmempty = to gmempty'
 
 class GSemigroup f => GMonoid f where
-  gmempty :: f p
+  gmempty' :: f p
 
 instance GMonoid U1 where
-  gmempty = U1
+  gmempty' = U1
 
 instance (Semigroup a, Monoid a) => GMonoid (K1 i a) where
-  gmempty = K1 mempty
+  gmempty' = K1 mempty
 
 instance GMonoid f => GMonoid (M1 i c f) where
-  gmempty = M1 gmempty
+  gmempty' = M1 gmempty'
 
 instance (GMonoid f, GMonoid g) => GMonoid (f :*: g) where
-  gmempty = gmempty :*: gmempty
+  gmempty' = gmempty' :*: gmempty'
